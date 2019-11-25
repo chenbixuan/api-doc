83ee1e97e066fe69dcb6b8b980d002615ac550ef
colter <89688563@qq.com>
2019-11-25 22:37:25 +0800

    .

--- api/wxUser.js
+++ api/wxUser.js
@@ -39,47 +39,6 @@ module.exports = {
           }
         }
       },
-      "put": {
-        "tags": ["wxUser"],
-        "summary": "更新会员信息",
-        "description": "wxUser",
-        "produces": [
-          "application/json"
-        ],
-        "parameters": [
-          {
-            "name": "wxUserId",
-            "in": "path",
-            "description": "会员id",
-            "required": true,
-            "type": "integer",
-            "format": "int64"
-          },
-          {
-            "name": "wxUser",
-            "in": "body",
-            "description": "会员信息",
-            "required": true,
-            "schema": {
-              "$ref": "#/definitions/WxUser"
-            }
-          }
-        ],
-        "responses": {
-          "200": {
-            "description": "successful operation",
-            "schema": {
-              "$ref": "#/definitions/WxUser"
-            }
-          },
-          "400": {
-            "description": "Invalid ID supplied"
-          },
-          "404": {
-            "description": "WxUser not found"
-          }
-        }
-      },
       "delete": {
         "tags": ["wxUser"],
         "summary": "删除某个会员",
@@ -136,6 +95,39 @@ module.exports = {
             "schema": {
               "type": "array",
               "$ref": "#/definitions/WxUserList"
+            }
+          },
+          "400": {
+            "description": "Invalid ID supplied"
+          },
+          "404": {
+            "description": "WxUser not found"
+          }
+        }
+      },
+      "put": {
+        "tags": ["wxUser"],
+        "summary": "更新会员信息",
+        "description": "wxUser",
+        "produces": [
+          "application/json"
+        ],
+        "parameters": [
+          {
+            "name": "wxUser",
+            "in": "body",
+            "description": "会员信息",
+            "required": true,
+            "schema": {
+              "$ref": "#/definitions/WxUser"
+            }
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "successful operation",
+            "schema": {
+              "$ref": "#/definitions/WxUser"
             }
           },
           "400": {
